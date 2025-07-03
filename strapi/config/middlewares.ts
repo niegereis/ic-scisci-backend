afdc6b6a29076d7373bde67b432c98d7407af2b5
niegereis <niegemr@gmail.com>
2025-07-03 16:49:12 -0300

    feat: cors configuration

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -2,16 +2,7 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:3000', 'https://ic-scisci-frontend.vercel.app/'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Accept'],
-      keepHeadersOnError: true,
-      expose: ['WWW-Authenticate', 'Server-Authorization'],
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
